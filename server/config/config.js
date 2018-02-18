068bd71aa92c7fb7048ef83f00044cee03051047
Sebastian <sabadus_sebastian@yahoo.com>
2018-02-18 23:56:27 +0200

    small change

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development'){
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/DrivingSchoolDB';
+  process.env.MONGODB_URI = 'mongodb://admin:Driving@ds141028.mlab.com:41028/driving-school-db';
 }else if(env === 'test'){
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/DrivingSchoolDB';
+  process.env.MONGODB_URI = 'mongodb://admin:Driving@ds141028.mlab.com:41028/driving-school-db';
 }
